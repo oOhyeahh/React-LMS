e0928a563a751de3503d9498051599623220ecaf
oOhyeahh <oliverye9606@gmail.com>
2018-02-21 18:35:46 +1100

    loader change

--- src/framework/UI/PageLoader/PageLoader.jsx
+++ src/framework/UI/PageLoader/PageLoader.jsx
@@ -5,7 +5,7 @@ import './page-loader.css';
 
 export default function PageLoader({ className, children, ...other }) {
   return (
-    <div className={classnames('ui segment jr-page-loader', className)} {...other}>
+    <div className={classnames('loader', className)} {...other}>
       <div className="ui active inverted dimmer">
         <div className="ui text loader">{children || 'Loading'}</div>
       </div>
